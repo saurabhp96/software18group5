4d60526bc1cc8653b0b5f30663406a5c696e48f8
Rohith Sivakumar <rohithsivakumar@gmail.com>
2018-03-14 16:46:54 -0400

    worked on login screen

--- app/src/main/java/com/example/softwareeng18/food4thought/MainActivity.java
+++ app/src/main/java/com/example/softwareeng18/food4thought/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.softwareeng18.food4thought;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView loginErrorWarning = (TextView) findViewById(R.id.loginErrorWarning);
+        loginErrorWarning.setText("");
+
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText usernameEditText = (EditText) findViewById(R.id.username);
+                EditText passwordEditText = (EditText) findViewById(R.id.password);
+                String enteredUsername = usernameEditText.getText().toString();
+                String enteredPassword = passwordEditText.getText().toString();
+                // check login credentials (enteredUsername and enteredPassword)
+
+//                if (login not successful)
+//                {
+//                    loginErrorWarning.setText("Login Unsuccessful");
+//                }
+//                else
+//                {
+//                    //move to employee/manager screen
+//                }
+
+            }
+        });
     }
 }
