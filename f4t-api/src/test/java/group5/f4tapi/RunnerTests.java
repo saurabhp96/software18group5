bd5fc4ea3fc149be5495ebbf01c6da09105c6550
unknown <akhilvelagapudi@gmail.com>
2018-03-18 13:28:10 -0400

    initialized spring api

---
+++ f4t-api/src/test/java/group5/f4tapi/RunnerTests.java
@@ -0,0 +1,16 @@
+package group5.f4tapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RunnerTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
