b33f8f47d320100c10a3e0b9ab858802ffbc1613
unknown <akhilvelagapudi@gmail.com>
2018-03-26 15:57:34 -0400

    unit testing structure

---
+++ f4t-api/src/test/java/group5/f4tapi/ControllerTests.java
@@ -0,0 +1,47 @@
+package group5.f4tapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ControllerTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Test
+    public void addUser() {
+    }
+
+    @Test
+    public void findAllCustomers() {
+    }
+
+    @Test
+    public void findItemsInOrder() {
+    }
+
+    @Test
+    public void findAllTables() {
+    }
+
+    @Test
+    public void findAllEmployees() {
+    }
+
+    @Test
+    public void findAllMenuItems() {
+    }
+
+    @Test
+    public void checkLogin() {
+    }
+
+    @Test
+    public void addEmployee() {
+    }
+}
