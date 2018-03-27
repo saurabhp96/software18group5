017ff6a32696490efdcaffead70f37cfa6c2adc9
Katheryne Zak-Strzalka <katherynezak-strzalka@Katherynes-MacBook-Pro.local>
2018-03-27 19:53:44 -0400

    updated pay bill, customer wait time, and order screens

---
+++ android/app/src/main/java/com/example/softwareeng18/food4thought/CustomerWaitTimeActivity.java
@@ -0,0 +1,13 @@
+package com.example.softwareeng18.food4thought;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomerWaitTimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_wait_time);
+    }
+}
