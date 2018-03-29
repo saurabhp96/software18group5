38e3fbfdfff05c0f799dec947807cfca32788b99
Rohith Sivakumar <rohithsivakumar@gmail.com>
2018-03-29 14:37:24 -0400

    SAMOSAS

--- android/app/src/main/java/com/example/softwareeng18/food4thought/OrderActivity.java
+++ android/app/src/main/java/com/example/softwareeng18/food4thought/OrderActivity.java
@@ -3,17 +3,16 @@
 
 package com.example.softwareeng18.food4thought;
 
-import android.os.Bundle;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -97,6 +96,7 @@ public class OrderActivity extends AppCompatActivity {
         listAdapter = new ArrayAdapter<String>(this, R.layout.orderrow, menuList);
         menuList.add("Alfredo Pasta");
         menuList.add("Spicy Sushi");
+        menuList.add("Samosas");
         menuView.setAdapter(listAdapter);
         listAdapter.notifyDataSetChanged();
 
