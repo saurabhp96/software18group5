ab6473ea8058f45940b4e0e7bf7e85e45dfa34c1
nithint29 <nithint29@gmail.com>
2018-03-28 20:30:07 -0400

    logging in

--- android/app/src/main/java/com/example/softwareeng18/food4thought/LoginActivity.java
+++ android/app/src/main/java/com/example/softwareeng18/food4thought/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.softwareeng18.food4thought;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,6 +29,7 @@ public class LoginActivity extends AppCompatActivity {
 
         final TextView loginErrorWarning = (TextView) findViewById(R.id.loginErrorWarning);
         loginErrorWarning.setText("");
+//        RequestQueue queue = Volley.newRequestQueue(this);
 
 
         Button loginButton = (Button) findViewById(R.id.loginButton);
@@ -26,6 +39,51 @@ public class LoginActivity extends AppCompatActivity {
                 EditText passwordEditText = (EditText) findViewById(R.id.password);
                 String enteredUsername = usernameEditText.getText().toString();
                 String enteredPassword = passwordEditText.getText().toString();
+
+                RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+                String url = "http://172.31.192.122:8080";
+                url = url+"/login?empid="+ enteredUsername + "&password=" + enteredPassword;
+
+// Request a string response from the provided URL.
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                if(response.trim().length()==0){
+                                    Toast toast = Toast.makeText(getApplicationContext(), "fail", Toast.LENGTH_SHORT);
+                                    toast.show();
+                                }
+                                else{
+
+                                    try {
+                                        JSONObject resp = new JSONObject();
+
+                                        resp = new JSONObject(response);
+                                        Toast toast = Toast.makeText(getApplicationContext(), resp.getString("role"), Toast.LENGTH_SHORT);
+                                        toast.show();
+                                    }
+                                    catch(Exception e){
+
+                                    }
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Context context = getApplicationContext();
+                        CharSequence text = "Server Error!";
+                        int duration = Toast.LENGTH_SHORT;
+
+                        Toast toast = Toast.makeText(context, text, duration);
+                        toast.show();
+                    }
+                });
+
+                queue.add(stringRequest);
+
+
+
                 // check login credentials (enteredUsername and enteredPassword)
 
 //                if (login not successful)
