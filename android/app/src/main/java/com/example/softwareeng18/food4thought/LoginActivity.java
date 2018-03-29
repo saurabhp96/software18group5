6dd77d037d20d4720aaea9c19a6e5be31c2fb08a
Rohith Sivakumar <rohithsivakumar@gmail.com>
2018-03-28 20:02:08 -0400

    worked more on screens, transitions

--- android/app/src/main/java/com/example/softwareeng18/food4thought/LoginActivity.java
+++ android/app/src/main/java/com/example/softwareeng18/food4thought/LoginActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,30 +13,30 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final TextView loginErrorWarning = (TextView) findViewById(R.id.loginErrorWarning);
-        loginErrorWarning.setText("");
-
-
-        Button loginButton = (Button) findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                EditText usernameEditText = (EditText) findViewById(R.id.username);
-                EditText passwordEditText = (EditText) findViewById(R.id.password);
-                String enteredUsername = usernameEditText.getText().toString();
-                String enteredPassword = passwordEditText.getText().toString();
-                // check login credentials (enteredUsername and enteredPassword)
-
-//                if (login not successful)
-//                {
-//                    loginErrorWarning.setText("Login Unsuccessful");
-//                }
-//                else
-//                {
-//                    //move to employee/manager screen
-//                }
-
-            }
-        });
+//        final TextView loginErrorWarning = (TextView) findViewById(R.id.loginErrorWarning);
+//        loginErrorWarning.setText("");
+//
+//
+//        Button loginButton = (Button) findViewById(R.id.loginButton);
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                EditText usernameEditText = (EditText) findViewById(R.id.username);
+//                EditText passwordEditText = (EditText) findViewById(R.id.password);
+//                String enteredUsername = usernameEditText.getText().toString();
+//                String enteredPassword = passwordEditText.getText().toString();
+//                // check login credentials (enteredUsername and enteredPassword)
+//
+////                if (login not successful)
+////                {
+////                    loginErrorWarning.setText("Login Unsuccessful");
+////                }
+////                else
+////                {
+////                    //move to employee/manager screen
+////                }
+//
+//            }
+//        });
 
 
         //delete code below
